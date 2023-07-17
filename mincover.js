d6bacb07296496456619dc8fb2331ea17fd878d9
Worm23 <p@w23.at>
2023-07-17 17:30:25 +0200

    Update mincover.js
    variable misnamed

--- mincover.js
+++ mincover.js
@@ -1,9 +1,9 @@
-// it seems like there is no way to stop the cover process while closing/opening 
+ // it seems like there is no way to stop the cover process while closing/opening 
 // when a certain minimum cover is reached. But at least it's possible to reset 
 // it to the minimum when it's not opening/closing anymore
 //
 // This script listens for the Status handler and only tries to reset the cover when a certain position is reached
-let minCover = 15;
+let minCover = 20;
 
 
 let startMonitor = false;
@@ -21,9 +21,9 @@ if (Shelly.getComponentConfig("sys").device.profile === "cover") {
             print(maxCover);
             print(e.delta.current_pos < maxCover);
       */
-      if (startMonitor === true && e.delta.current_pos < maxCover) {
+      if (startMonitor === true && e.delta.current_pos < minCover) {
         print("Cover moved to < 15% -> stop it");
-        Shelly.call("Cover.GoToPosition", {'id': 0, 'pos': maxCover});
+        Shelly.call("Cover.GoToPosition", {'id': 0, 'pos': minCover});
       }
       
       if (e.delta.state === "stopped" || e.delta.state === "closed" || e.delta.state === "open") {
